3d577a6b7cd96f8fdc9abfa7dd43a70fee3afcd1
esubaalew <esubalewchekol6@gmail.com>
2025-02-04 23:43:43 +0300

    fixed a bug

--- src/my_py_pkg/my_py_pkg/number_publisher.py
+++ src/my_py_pkg/my_py_pkg/number_publisher.py
@@ -6,6 +6,7 @@ from example_interfaces.msg import Int64
 class NumberPublisher(Node):
     def __init__(self):
         super().__init__('number_publisher')
+        self.number_ = 2
         self.number_publisher_ = self.create_publisher(Int64, "number", 10)
         self.number_timer_ = self.create_timer(1.0, self.publish_number)
         self.get_logger().info("Number publisher has been started")
@@ -23,3 +24,7 @@ def main(args=None):
     node = NumberPublisher()
     rclpy.spin(node)
     rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
